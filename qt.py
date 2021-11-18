ef986fb6989374bdb37cac226f0491941e923712
bastian99 <bastianser924@gmail.com>
2021-11-17 21:36:55 -0600

    eq

--- qt.py
+++ qt.py
@@ -151,7 +151,7 @@ st.latex(r'\\')
 '''
 2.27 The time-independent wave function of a particle of mass m moving in a potential V(x)= α²x² is
 '''
-st.latex(r'\psi (x) = \exp \left( {\sqrt {\frac{{m{\alpha ^2}}}{{2{\hbar ^2}}}} {x^2}} \right),{\hspace 0.5cm}\alpha being{\hspace 0.5cm}a{\hspace 0.5cm}constant')
+st.latex(r'\psi (x) = \exp \left( {\sqrt {\frac{{m{\alpha ^2}}}{{2{\hbar ^2}}}} {x^2}} \right),{\hspace 0.2cm}\alpha{\hspace 0.2cm} being{\hspace 0.2cm}a{\hspace 0.2cm}constant')
 '''
 Find the energy of the system
 '''
